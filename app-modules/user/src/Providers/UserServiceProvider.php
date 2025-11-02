917805a306e9d9f87e9e9052326bb9bf72042e57
ranyesantos <lucasgts17@gmail.com>
2025-11-02 11:37:06 -0300

    feat(user module): base user module

---
+++ app-modules/user/src/Providers/UserServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Helpz\User\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
