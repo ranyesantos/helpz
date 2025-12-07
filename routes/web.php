20fecc30b60c9c928ecbe02192f75c6044b36748
ranyesantos <lucasgts17@gmail.com>
2025-12-07 18:30:46 -0300

    feat: add log ping for testing

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test-log', function () {
+    Log::debug('log custom debug');
+    Log::info('log info');
+    Log::warning('log warning');
+    Log::error('log error');
+
+    return 'ok';
+});
