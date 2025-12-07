20e9291bc60d9c312698e50d9a12ac3d1c5ae501
ranyesantos <lucasgts17@gmail.com>
2025-12-07 18:32:04 -0300

    feat: telescope config

--- config/telescope.php
+++ config/telescope.php
@@ -176,7 +176,7 @@ return [
 
         Watchers\LogWatcher::class => [
             'enabled' => env('TELESCOPE_LOG_WATCHER', true),
-            'level' => 'debug',
+            'level' => 'all',
         ],
 
         Watchers\MailWatcher::class => env('TELESCOPE_MAIL_WATCHER', true),
