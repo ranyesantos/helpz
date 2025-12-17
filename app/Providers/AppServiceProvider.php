999065a3da65c27714dc479f38bf50df586d3e12
ranyesantos <lucasgts17@gmail.com>
2025-12-17 17:12:13 -0300

    setup: enhance the system's setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,14 +15,54 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->registerDebugbar();
+    }
+    
+    /**
+     * Bootstrap any application services.
+    */
+    public function boot(): void
+    {
+        $this->configureVite();
+        $this->configureDatabase();
+        $this->configureCommands();
+    }
+    
+    /**
+     * Configure the application's Vite
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 
     /**
-     * Bootstrap any application services.
+     * Configure the application's commands
      */
-    public function boot(): void
+    private function configureCommands(): void
     {
-        //
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+    }
+
+    /**
+    * Configure the application's models
+    */
+    private function configureDatabase(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    private function registerDebugbar(): void
+    {
+        if (
+            app()->isLocal()
+            && app()->hasDebugModeEnabled()
+            && class_exists(\Barryvdh\Debugbar\ServiceProvider::class)
+            && config('debugbar.enabled')
+        ) {
+            Log::notice('Registering Debugbar Service Provider');
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
